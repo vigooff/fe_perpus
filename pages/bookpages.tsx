c8af6af9ff27f12e006095ad572065706ad7d734
Vigo Satria Ramadhan <vgooffical@gmail.com>
2026-06-16 22:29:51 +0700

    Add BookPages component with development message

---
+++ pages/bookpages.tsx
@@ -0,0 +1,8 @@
+export default function BookPages() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Halaman Buku</h1>
+      <p>Halaman ini sedang dalam tahap pengembangan.</p>
+    </div>
+  );
+}
